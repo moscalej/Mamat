8c5b66fea86d4251790b5ce57a5cb2941451a5ca
YaronWhite86 <yaronwhite86@gmail.com>
2017-01-24 21:12:31 +0200

    Need to add copy constructor.
    I'm having trouble passing values like in main1.cpp line 48

--- HW5/Code/TempVec.h
+++ HW5/Code/TempVec.h
@@ -7,63 +7,120 @@ enum Result { FAILURE, SUCCESS };
 
 using namespace std;
 
-template <class T,int size>
+
+template <class T, int size>
 class TempVec {
-	public:
-		// Default Constructor
-		TempVec()
+
+public:
+	//Default Constructor
+	TempVec();
+	//TempVec(const TempVec& a);
+
+	// COverload "="
+	/*TempVec& operator=(const TempVec& a)
+	{
+	if (this != &a)
+	{
+	delete[] this->coordinates_ ;
+	copy(a);
+	}
+	return (*this);
+	}*/
+	TempVec& operator=(const T& rhs)
+	{
+		TempVec<T, size> temp;
+		temp.coordinates_[index_] = rhs;
+		return *this;
+	}
+	// Constructor for 3D coordinates
+	TempVec(int x, int y, int z);
+
+	// Default Destructor
+	virtual ~TempVec() { }
+
+
+	// Operator Overloaders '+', '*' , '-' , '='
+	TempVec operator+(const TempVec& rhs);
+	TempVec operator*(const TempVec& rhs);
+	TempVec operator-(const TempVec& rhs);
+	TempVec operator*(T rhs);
+	TempVec operator[](T rhs);
+	//TempVec operator=(T rhs);
+	friend TempVec operator* (const T lhs, const TempVec& rhs) // Allows left side multiplication.
+	{
+		TempVec<T, size> temp;
+		int i = 0;
+		while (i < size)
 		{
-			try 
-			{
-				for (int i = 0; i < size; i++)
-				{
-					if (i > size || i < 0) throw "Error TempVec. Illegal index";
-					coordinates_[i] = 0;
-				}
-			}
-			catch (const string errMessage)
-			{
-				//cerr << errMessage << endl;
-			}
+			temp.coordinates_[i] = rhs.coordinates_[i] * lhs;
+			i++;
 		}
+		return temp;
+	}
 
-		// Constructor for 3D coordinates
-		TempVec(int x, int y, int z);
 
-		// Default Destructor
-		virtual ~TempVec() {}
+	//Setting values.
+	Result SetVector(char* Nth);
+	Result SetCoordinate(int num);
+	T GetCoordinate(int num);
 
-		// Operator Overloaders '+', '*' 
-		TempVec<T, size> operator+(const TempVec<T, size>& rhs);
-		TempVec<T, size> operator*(const TempVec<T, size>& rhs);
+	//Vector Math
+	friend int InnerProduct(TempVec u, TempVec v);
+	friend int SqNorm(TempVec u);
+	friend int SqDistance(TempVec u);
 
-		// Vector Math
-		void addVectors(T vec1, T vec2);
-		void subtractVectors(T vec1, T vec2);
-		void multiply_by_scalar(int num, T vec1);
-		//int InnerProduct(int u, int v);
-		//int SqNorm(int u);
-		//int SqDistance(int u);
-		
-		void print();
-
-	protected:
-		
-		T coordinates_[size];
+	void print();
+protected:
+	int count_;// To count how many vectors we have created.
+	int index_;
+	T coordinates_[size];
+	T* p_coordinates_ = coordinates_;
 };
 
+// Default Constructor with exception handling
+template <class T, int size>
+TempVec<T, size>::TempVec()
+{
+	count_ = 0;
+	index_ = 0;
+	try
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			if (i > 3 || i < 0) throw "Error TempVec. Illegal index";
+			coordinates_[i] = 0;
+		}
+	}
+	catch (const char* errMessage)
+	{
+		cerr << "Error TempVec. " << errMessage << endl;
+	}
+}
+
+// Constructor...
 template <class T, int size>
 TempVec<T, size>::TempVec(int x, int y, int z)
 {
+	this->count_ = 0;
+	this->index_ = 0;
 	this->coordinates_[0] = x;
 	this->coordinates_[1] = y;
 	this->coordinates_[2] = z;
 }
+
+// Copy Constructor
+/*
+template <class T, int size>
+TempVec<T, size>::TempVec(const TempVec& a)
+{
+copy(a);
+}*/
+
 //***********************************************************************
 //* Operator overload for addition ("+")
 //***********************************************************************
 template <class T, int size>
-TempVec<T, size> TempVec<T, size>::operator+(const TempVec<T, size>& rhs) 
+TempVec<T, size> TempVec<T, size>::operator+(const TempVec<T, size>& rhs)
 {
 	TempVec<T, size> temp;
 	int i = 0;
@@ -108,6 +165,109 @@ TempVec<T, size> TempVec<T, size>::operator*(const TempVec<T, size>& rhs)
 }
 
 //***********************************************************************
+//* Operator overload for multiplication ("*") rhs = T, lhs = TempVec
+//***********************************************************************
+template <class T, int size>
+TempVec<T, size> TempVec<T, size>::operator*(T rhs)
+{
+	TempVec<T, size> temp;
+	int i = 0;
+	while (i < size)
+	{
+		temp.coordinates_[i] = coordinates_[i] * rhs;
+		i++;
+	}
+	return temp;
+}
+
+//***********************************************************************
+//* Operator overload for access ("[]") rhs = T
+//***********************************************************************
+template <class T, int size>
+TempVec<T, size> TempVec<T, size>::operator[](T rhs)
+{
+	this->index_ = rhs;
+
+	return *this;
+}
+
+
+//***********************************************************************
+//* SetVector()
+//* Method for initializing a vector
+//* Parameters: string Nth
+//* Returns VOID
+//***********************************************************************
+template <class T, int size>
+Result TempVec<T, size>::SetVector(char* Nth)
+{
+	int i = Nth[0] - '0';
+
+	if (i < 0) return FAILURE;
+
+	this->count_ = i - 1;
+	return SUCCESS;
+}
+
+//***********************************************************************
+//* SetCoordinate() PROBABLY DON'T NEED
+//* Method for initializing a vectors coordinates
+//* Parameters: string Nth
+//* Returns VOID
+//***********************************************************************
+template <class T, int size>
+Result TempVec<T, size>::SetCoordinate(int num)
+{
+	try
+	{
+		if (index_ > 2) throw "Error TempVec. Illegal index";
+		//if (index_ == size) index_ = 0; // Back to the beginning of the vector
+		if (index_ == 0)
+		{
+			coordinates_[0] = num;
+			index_++;
+			return SUCCESS;
+		}
+		if (index_ == 1)
+		{
+			coordinates_[1] = num;
+			index_++;
+			return SUCCESS;
+		}
+		if (index_ == 2)
+		{
+			coordinates_[2] = num;
+			index_++;
+			return SUCCESS;
+		}
+	}
+	catch (const char* errMessage) {
+		cerr << errMessage << endl;
+	}
+	return FAILURE;
+}
+
+//***********************************************************************
+//* GetCoordinate()
+//* Method for getting a vectors coordinates
+//* Parameters: int num
+//* Returns T
+//***********************************************************************
+template <class T, int size>
+T TempVec<T, size>::GetCoordinate(int num)
+{
+	try
+	{
+		if (num>2) throw "Error TempVec. Illegal index";
+
+	}
+	catch (const char* errMessage) {
+		cerr << errMessage << endl;
+	}
+	return coordinates_[num];
+}
+
+//***********************************************************************
 //* print()
 //* Method for printing 
 //* Parameters: None
@@ -117,36 +277,71 @@ template <class T, int size>
 void TempVec<T, size>::print()
 {
 	int i = 0;
+	cout << "v is " << "(";
 	while (i < size)
 	{
-		cout << coordinates_[i] << endl;
-		i++;
+		if (i == size - 1)
+		{
+			cout << coordinates_[i];
+			//cout << "," << endl;
+			i++;
+		}
+		else
+		{
+			cout << coordinates_[i] << ",";
+			i++;
+		}
 	}
+	cout << ")" << endl;
+}
+
+
+//***********************************************************************
+//* InnerProduct()
+//* Exterior function for getting the Inner Product of two vectors
+//* Parameters: TempVec vec1, TempVec vec2
+//* Returns int
+//***********************************************************************
+int InnerProduct(TempVec<int, 3> vec1, TempVec<int, 3> vec2)
+{
+	TempVec<int, 3> temp = vec1*vec2;
+	int theSum = 0;
+	for (int i = 0; i < 3; i++) {
+
+		theSum += temp.GetCoordinate(i);
+	}
+	return theSum;
 }
 
 //***********************************************************************
-//* print()
-//* Method for printing 
-//* Parameters: None
-//* Returns VOID
+//* SqNorm()
+//* Exterior function for getting the norma squared (||u||^2) of a vector
+//* Parameters: TempVec vec1
+//* Returns int
 //***********************************************************************
-template <class T, int size>
-void TempVec<T, size>::addVectors(T vec1, T vec2)
+int SqNorm(TempVec<int, 3> vec1)
 {
+	TempVec<int, 3> temp = vec1*vec1;
+	int theSum = 0;
+	for (int i = 0; i < 3; i++) {
 
-}
-
-
-
-
-/*
-namespace vectorFunctions
-{
-	int InnerProduct(T* u, T* v)
-	{
-		for (int i = 0; i < 3 ;i++)
+		theSum += temp.GetCoordinate(i);
 	}
+	return theSum;
 }
-*/
+
+//***********************************************************************
+//* SqDistance()
+//* Exterior function for getting the distance between two vectors
+//* Parameters: TempVec vec1 TempVec vec2
+//* Returns int
+//***********************************************************************
+int SqDistance(TempVec<int, 3> vec1, TempVec<int, 3> vec2)
+{
+	TempVec<int, 3> temp = vec1 - vec2;
+
+	return SqNorm(temp);
+}
+
 
 #endif
--- HW5/Code/main1.cpp
+++ HW5/Code/main1.cpp
@@ -21,19 +21,45 @@
 using namespace std;
 
 typedef TempVec<int, 3> Integer3;
+
 typedef TempVec<complex, 2> Complex2;
 
 void main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	
+	Integer3* int3_list = new Integer3[3];
 	Integer3 vector1(1, 1, 1);
 	Integer3 vector2(2, 2, 2);
-	Integer3 vector3;
-	vector3 = vector1 * vector2;
-	vector3.print();
-	//Complex2 complex1;
 	
+	Integer3 vector3 = vector1 * vector2 + vector2 - vector1;
+	Integer3 vector4 = 5*vector1*5;
+	Integer3 vector5(2,2,2);
+	int k = InnerProduct(vector1, vector2);
+	cout << "k is " << k << endl;
+	int p = SqNorm(vector1);
+	cout << "p is " << p << endl;
+	int q = SqDistance(vector3, vector1);
+	cout << "q is " << q << endl;
+	Result t;
+	int x = 10;
+	int3_list[0] = vector5;
+	int3_list[0].print();
+
+	int3_list[0][0] = x;// vector1.GetCoordinate(0);
+	x = 9;
+	int3_list[0][1] = x;
+	x = 8;
+	int3_list[0][2] = x;
+	
+	
+	int3_list[0].print();
+	vector1.print();
+	vector2.print();
+	vector3.print();
+	vector4.print();
+	
+	
+	delete[] int3_list;
 
 	_CrtDumpMemoryLeaks();
 	return;
